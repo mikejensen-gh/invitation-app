db3c5479ca21d61f0ab64c8972d31109c2df4192
mikejensen-gh <michael@bidray.com>
2018-09-18 11:27:13 -0700

    Added: edit invitee name functionality

---
+++ src/components/InviteeName.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class InviteeName extends Component {
+  render() {
+    if (this.props.nameEditable) {
+      return <input
+        type="text"
+        value={this.props.name}
+        onChange={(e) => this.props.handleChange(e.target.value)}
+      />
+    } else {
+      return <span>{this.props.name}</span>
+    }
+  }
+}
