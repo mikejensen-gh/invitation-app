25fa90de869f7d939aa1e8592b239a75cef25241
mikejensen-gh <michael@bidray.com>
2018-09-19 09:19:17 -0700

    Added: pending invitee functionality

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Header from './components/Header.js'
 import './App.css'
 import Invitee from './components/Invitee.js'
-import Counter from './components/Counter.js'
+import AttendanceCount from './components/AttendanceCount.js'
 import Subheader from './components/Subheader.js'
+import PendingInvitee from './components/PendingInvitee.js'
 
 export default class App extends Component {
   constructor(props) {
@@ -11,7 +12,8 @@ export default class App extends Component {
 
     this.state = {
       invitees: this.props.initialInvitees,
-      hideUnresponded: false
+      hideUnresponded: false,
+      pendingInviteeName: 'Saria'
     }
 
     this.toggleHideUnresponded = this.toggleHideUnresponded.bind(this)
@@ -54,19 +56,47 @@ export default class App extends Component {
     })
   }
 
+  handlePendingInviteeNameChange(name) {
+    this.setState({
+      pendingInviteeName: name
+    })
+  }
+
+  handleNewInviteeSubmit() {
+    const invitees = [...this.state.invitees]
+
+    invitees.push({
+      key: this.state.invitees.length,
+      name: this.state.pendingInviteeName,
+      confirmed: false
+    })
+
+    this.setState({
+      invitees: invitees,
+      pendingInviteeName: ''
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header
+          inputValue={this.state.pendingInviteeName}
+          handleChange={(name) => this.handlePendingInviteeNameChange(name)}
+          handleSubmit={this.handleNewInviteeSubmit.bind(this)}
+        />
         <div className="main">
           <Subheader
             hideUnresponded={this.state.hideUnresponded}
             toggleHideUnresponded={this.toggleHideUnresponded}
           />
-          <Counter
+          <AttendanceCount
             invitees={this.state.invitees}
           />
           <ul>
+            <PendingInvitee
+              name={this.state.pendingInviteeName}
+            />
             {this.state.invitees
               .filter(invitee => !this.state.hideUnresponded || invitee.confirmed)
               .map((invitee, index) =>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.props.handleSubmit()
+  }
+
   render() {
     return (
       <header>
         <h1>RSVP</h1>
         <p>A Treehouse App</p>
-        <form>
-          <input type="text" defaultValue="Safia" placeholder="Invite Someone"/>
-            <button type="submit" name="submit" value="submit">Submit</button>
+        <form
+          onSubmit={this.handleSubmit.bind(this)}
+        >
+          <input
+            type="text"
+            value={this.props.inputValue}
+            placeholder="Invite Someone"
+            onChange={(e) => this.props.handleChange(e.target.value)}
+          />
+          <button type="submit" name="submit" value="submit">Submit</button>
         </form>
       </header>
     );
